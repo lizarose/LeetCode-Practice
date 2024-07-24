a5c1a879746f8131731546a8c521fcf5063ac3f1
Elizabeth Levinson <lizarose266@gmail.com>
2024-07-24 09:22:09 -0500

    Added notes for Immediately Invoked Function Expression

--- JavaScript/Closures/Day1-HelloWord.js
+++ JavaScript/Closures/Day1-HelloWord.js
@@ -19,3 +19,14 @@ console.log(createHelloWorld()); // Output: "Hello World"
     }
     console.log(add(3, 4)) //Output: 7
     
+
+    //* Immediately Invoked Function Expression --> you can create a function and immediately execute it
+    console.log("\nThis is an Immediately Invoked Function Expression where the numbers being entered for the parameter are entered at the end of the function, not in a console log.")
+    let result = function(a, b) {
+        const sum = a + b;
+        return sum;
+    } (4, 4);
+    console.log(result); //Output: 8
+    //* You write code like this to encapsulate a variable within a new scope. For example, another dev can immediately see the 'sum' can not be used anywhere outside the function body.
+
+    
