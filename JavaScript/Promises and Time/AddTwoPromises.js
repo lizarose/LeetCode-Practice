09b0d5dac96c8e0dda008066b8129882e029ec21
Elizabeth Levinson <lizarose266@gmail.com>
2024-08-13 09:14:39 -0500

    Created promises

---
+++ JavaScript/Promises and Time/AddTwoPromises.js
@@ -0,0 +1,7 @@
+//TODO: Given two promises promise1 and promise2, return a new promise. promise1 and promise2 will both resolve with a number. The returned promise should resolve with the sum of the two numbers.
+
+
+var addTwoPromises = async function(promise1, promise2) {
+    return Promise.all([promise1, promise2])
+        .then(values => values[0] + values[1]);
+};
